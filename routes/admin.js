3f0bd21ca3bde5e8383861e0fdea7bfc8e5aee90
Sushmitameena <95956469+Sushmitameena@users.noreply.github.com>
2025-03-22 10:39:49 +0530

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,7 @@ const express = require('express');
 const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router();
 
+
 router.get('/', authMiddleware(['admin']), (req, res) => {
   res.json({ msg: 'Admin dashboard', user: req.user });
 });
