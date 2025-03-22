c4f750f6600abb353ec5366a4ba1a316c9803e91
Sushmitameena <95956469+Sushmitameena@users.noreply.github.com>
2025-03-22 10:36:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,4 +16,4 @@ app.use('/api/profile', require('./routes/profile'));
 app.use('/api/admin', require('./routes/admin'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
